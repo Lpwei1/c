bbe5b436d912481c84e79b47a1ef02ec9b4ab7a4
Lpwei1 <1575339637@qq.com>
2021-04-19 16:36:39 +0800

    .

---
+++ _init_.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 19 15:35:21 2021
+
+@author: weilp111
+"""
+
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 19 15:32:40 2021
+
+@author: weilp111
+"""
+
+
+from setuptools import setup
+setup(name='dingding-py',
+   version='0.0.2',
+   description='this is a dingdingbot packages',
+   author='Jruing',
+   author_email='1099301992@qq.com',
+   # packages=['Qingolddriver']
+ 
+   )
